16ef9ea1ef3a1ba643137a9b413e53eefb91b323
BimalKanxa <sharmabimal2003@gmail.com>
2023-10-18 11:45:57 +0530

    completed now

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import './App.css'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import NavBar from './components/Navbar'
 import About from './components/About'
 
